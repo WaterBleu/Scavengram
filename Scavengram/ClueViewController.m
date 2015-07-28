b46b759ee8789f2b1ec3adf867fb65ba4ec2c143
WaterBleu <jeff7746@hotmail.com>
2015-07-28 15:15:15 -0700

    Include check for array size

--- Scavengram/ClueViewController.m
+++ Scavengram/ClueViewController.m
@@ -31,60 +31,61 @@
 - (void) retrieveClue{
     NSString *apiURL = @"https://api.flickr.com/services/rest/?api_key=5f834de364c936e23556add640bc4ee8&format=json&photo_id=%@&method=flickr.photos.getsizes&nojsoncallback=1";
     
-    
-    for(int i = 1; i < _photoIDArray.count; i++){
-        NSURL *targetURL = [[NSURL alloc] initWithString:[NSString stringWithFormat:apiURL,_photoIDArray[i]]];
-        // retrieving the remaining item in the array and download in background
-        
-        NSURLSession *session = [NSURLSession sharedSession];
-        NSURLRequest *request = [[NSURLRequest alloc] initWithURL:targetURL];
-        NSURLSessionTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-            if (!error){
-                NSError *jsonError = nil;
-                
-                NSDictionary *retrievedPhotoDict = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
-                
-                NSArray *retrievedPhotos = [retrievedPhotoDict valueForKeyPath:@"sizes.size"];
-                
-                NSURL *imageURL;
-                for(NSDictionary *dict in retrievedPhotos){
-                    if ([dict[@"label"] isEqualToString:@"Medium"]) {
-                        imageURL = [[NSURL alloc] initWithString:dict[@"source"]];
+    if(_photoIDArray.count > 1){
+        for(int i = 1; i < _photoIDArray.count; i++){
+            NSURL *targetURL = [[NSURL alloc] initWithString:[NSString stringWithFormat:apiURL,_photoIDArray[i]]];
+            // retrieving the remaining item in the array and download in background
+            
+            NSURLSession *session = [NSURLSession sharedSession];
+            NSURLRequest *request = [[NSURLRequest alloc] initWithURL:targetURL];
+            NSURLSessionTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                if (!error){
+                    NSError *jsonError = nil;
+                    
+                    NSDictionary *retrievedPhotoDict = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+                    
+                    NSArray *retrievedPhotos = [retrievedPhotoDict valueForKeyPath:@"sizes.size"];
+                    
+                    NSURL *imageURL;
+                    for(NSDictionary *dict in retrievedPhotos){
+                        if ([dict[@"label"] isEqualToString:@"Medium"]) {
+                            imageURL = [[NSURL alloc] initWithString:dict[@"source"]];
+                        }
                     }
+                    
+                    NSString *apiURL = @"https://api.flickr.com/services/rest/?api_key=5f834de364c936e23556add640bc4ee8&format=json&photo_id=%@&method=flickr.photos.geo.getlocation&nojsoncallback=1";
+                    
+                    NSURL *targetURL = [[NSURL alloc] initWithString:[NSString stringWithFormat:apiURL,[_photoIDArray firstObject]]];
+                    
+                    NSURLSession *session = [NSURLSession sharedSession];
+                    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:targetURL];
+                    NSURLSessionTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                        if (!error){
+                            NSError *jsonError = nil;
+                            
+                            NSDictionary *retrievedPhotoIDDict = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+                            
+                            double retrievedPhotoLat = [[retrievedPhotoIDDict valueForKeyPath:@"photo.location.latitude"] doubleValue];
+                            double retrievedPhotoLng = [[retrievedPhotoIDDict valueForKeyPath:@"photo.location.longitude"] doubleValue];
+                            
+                            NSURLSession *session = [NSURLSession sharedSession];
+                            NSURLRequest *request = [[NSURLRequest alloc] initWithURL:imageURL];
+                            NSURLSessionDownloadTask *dataTask = [session downloadTaskWithRequest:request completionHandler:^(NSURL *location, NSURLResponse *response, NSError *error) {
+                                if(!error){
+                                    UIImage *image = [UIImage imageWithContentsOfFile:[NSString stringWithFormat:@"%@", location]];
+                                    GeoPhoto *geoPhoto = [[GeoPhoto alloc]initWithImage:image andLat:retrievedPhotoLat andLng:retrievedPhotoLng];
+                                    [_imageArray addObject:geoPhoto];
+                                    NSLog(@"Downloading in background! %dth  Geophoto added, %@",i , geoPhoto);
+                                }
+                            }];
+                            [dataTask resume];
+                        }
+                    }];
+                    [dataTask resume];
                 }
-                
-                NSString *apiURL = @"https://api.flickr.com/services/rest/?api_key=5f834de364c936e23556add640bc4ee8&format=json&photo_id=%@&method=flickr.photos.geo.getlocation&nojsoncallback=1";
-                
-                NSURL *targetURL = [[NSURL alloc] initWithString:[NSString stringWithFormat:apiURL,[_photoIDArray firstObject]]];
-                
-                NSURLSession *session = [NSURLSession sharedSession];
-                NSURLRequest *request = [[NSURLRequest alloc] initWithURL:targetURL];
-                NSURLSessionTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-                    if (!error){
-                        NSError *jsonError = nil;
-                        
-                        NSDictionary *retrievedPhotoIDDict = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
-                        
-                        double retrievedPhotoLat = [[retrievedPhotoIDDict valueForKeyPath:@"photo.location.latitude"] doubleValue];
-                        double retrievedPhotoLng = [[retrievedPhotoIDDict valueForKeyPath:@"photo.location.longitude"] doubleValue];
-                        
-                        NSURLSession *session = [NSURLSession sharedSession];
-                        NSURLRequest *request = [[NSURLRequest alloc] initWithURL:imageURL];
-                        NSURLSessionDownloadTask *dataTask = [session downloadTaskWithRequest:request completionHandler:^(NSURL *location, NSURLResponse *response, NSError *error) {
-                            if(!error){
-                                UIImage *image = [UIImage imageWithContentsOfFile:[NSString stringWithFormat:@"%@", location]];
-                                GeoPhoto *geoPhoto = [[GeoPhoto alloc]initWithImage:image andLat:retrievedPhotoLat andLng:retrievedPhotoLng];
-                                [_imageArray addObject:geoPhoto];
-                                NSLog(@"Downloading in background! %dth  Geophoto added, %@",i , geoPhoto);
-                            }
-                        }];
-                        [dataTask resume];
-                    }
-                }];
-                [dataTask resume];
-            }
-        }];
-        [dataTask resume];
+            }];
+            [dataTask resume];
+        }
     }
 }
 
